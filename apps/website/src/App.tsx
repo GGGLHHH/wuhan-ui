eb1eac585b9311a33b3f90dbeda5643735f8dc76
ge Datou <1635656638@qq.com>
2026-03-19 17:46:20 +0800

    update

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -1,10 +1,10 @@
-import { Silk } from '@gedatou/ui'
+import { Aurora } from '@gedatou/ui'
 
 export function App() {
   return (
     <div className="relative h-screen w-full bg-[#060010]">
-      {/* <Aurora colorStops={['#5227FF', '#5b117e', '#42119c']} amplitude={1} blend={1} /> */}
-      <Silk speed={5} scale={1} color="#d8135b" noiseIntensity={1.5} rotation={0} />
+      <Aurora colorStops={['#5227FF', '#5b117e', '#42119c']} amplitude={1} blend={1} />
+      {/* <Silk speed={5} scale={1} color="#d8135b" noiseIntensity={1.5} rotation={0} /> */}
     </div>
   )
 }
