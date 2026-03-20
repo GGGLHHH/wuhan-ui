6599b436ee5983559d08f865e9b79f5d87a2291d
ge Datou <1635656638@qq.com>
2026-03-21 01:52:50 +0800

    update transitions

--- apps/website/src/main.tsx
+++ apps/website/src/main.tsx
@@ -5,9 +5,15 @@ import { createRoot } from 'react-dom/client'
 import { routeTree } from './routeTree.gen'
 
 import './index.css'
+import './styles/transitions.css'
 import '@gedatou/ui/styles.css'
 
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+  defaultViewTransition: {
+    types: ['scale-fade'],
+  },
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
